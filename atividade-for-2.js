0a8a95fb767ec0e2456032459d880cf4235f386c
Vinnidias <vinicius.3991@gmail.com>
2020-02-19 21:20:40 -0300

    correçoes de variaveis desnecessarias

--- atividade-for-2.js
+++ atividade-for-2.js
@@ -1,6 +1,6 @@
 var user = require("readline-sync")
 var num1 = user.questionInt("digite um numero: ")
 var num2 = user.questionInt("digite outro numero: ")
-for(num1 ; num1<num2; num1++){
+for( num1<num2; num1++){
     console.log(num1)
 }
--- atividade-if-4.js
+++ atividade-if-4.js
@@ -2,8 +2,9 @@ var user = require("readline-sync")
 var macas = user.questionInt("quantas macas vc comprou: ")
 var macasvarejo = 0.3
 var macaatacado = 0.25
-if(macas<=12){
+var minatacado = 12
+if(macas<=minatacado){
     console.log(macas*macasvarejo)
-}else if(macas>12){
+}else {
         console.log(macas*macaatacado)
     }
