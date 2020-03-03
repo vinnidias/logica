3a7a9b2fcf4d7a8ad7d11841cf4013ca2fc0d214
Vinnidias <vinicius.3991@gmail.com>
2020-03-03 11:40:37 -0300

    atividade 4 lista 9

---
+++ atividade-9-4.js
@@ -0,0 +1,19 @@
+//Faça um procedimento que recebe, por parâmetro, 
+//um valor N e calcula e escreve a tabuada de 1 até N. Mostre a tabuada na forma:
+//1 x N = N
+//2 x N = 2N
+//...
+//N x N = N2
+//Após isso solicite o usuário um número e forneça o argumento do usuário para a função.
+
+
+var user = require("readline-sync")
+var numN = user.questionInt("digite um numero inteiro: ")
+
+
+function Tabuada(numN){
+   for(i=1;i<=numN;i++){
+    var resultado = i*numN
+    console.log(resultado)
+   } return console.log(resultado)
+}console.log(Tabuada(numN))
